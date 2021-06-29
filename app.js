c0afdfb4f3dd60bb766e566e68929522b07c4dc0
mittalaakash <mit.ash.619@gmail.com>
2021-06-29 23:03:41 +0530

    add  base template  header

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,6 +14,9 @@ const userRouter = require(`./routes/userRoutes`);
 const reviewRouter = require('./routes/reveiwRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 //1 Global Middlewares
 //set security http headers
@@ -54,7 +58,7 @@ app.use(
 );
 
 //serving static files
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //test middleware
 app.use((req, res, next) => {
@@ -65,6 +69,13 @@ app.use((req, res, next) => {
 
 //Routes
 //using subrouter as middleware
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter); //mounting router
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
