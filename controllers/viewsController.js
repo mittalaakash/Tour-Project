44de9da6c47f9d815b1c053e75713648d71a406d
mittalaakash <mit.ash.619@gmail.com>
2021-07-01 01:12:28 +0530

    create pug overview

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,20 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../Utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // Get tour data from collection
+  const tours = await Tour.find();
+  // Build template
+  //render that template using tour data form 1
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
   });
-};
+});
